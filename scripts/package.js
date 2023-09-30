3f6db200e43d238b5650a3482221fdad019c7aac
aYukiYoshida <yuki.yoshida@access-company.com>
2023-10-01 00:40:42 +0900

    Refactor script to package

--- scripts/package.js
+++ scripts/package.js
@@ -1,39 +1,40 @@
-/* eslint-disable */
-const fs = require('fs');
-const child_process = require('child_process');
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
+/* eslint-env node */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require ('fs');
+const readline = require ('readline').createInterface ({
+  input: process.stdin,
+  output: process.stdout,
 });
-const package = require('../package.json');
+const pkg = require ('../package.json');
+/* eslint-enable @typescript-eslint/no-var-requires */
 
 const confirmToProceed = (message, callback) => {
-    readline.question(`${message} (y/n): `, (answer) => {
-        if (answer.toLowerCase() !== 'y') {
-            readline.close();
-            console.log('Cancelled package');
-            return;
-        }
-
-        callback();
-    });
-};
-
-console.log(`Current package version: ${package.version}`);
-
-confirmToProceed('Is the set version correct for this release?', () => {
-    const releasePackage = { ...package };
-    delete releasePackage.scripts;
-
-    try {
-        fs.writeFileSync('dist/package.json', JSON.stringify(releasePackage));
-        fs.copyFileSync('README.md', 'dist/README.md');
-        fs.copyFileSync('LICENSE', 'dist/LICENSE');
-
-        console.log('Packaged');
-    } catch (err) {
-        console.error(err);
+  readline.question (`${message} (y/n): `, answer => {
+    if (answer.toLowerCase () !== 'y') {
+      readline.close ();
+      console.log ('Cancelled package');
+      return;
     }
 
-    readline.close();
+    callback ();
+  });
+};
+
+console.log (`Current package version: ${pkg.version}`);
+
+confirmToProceed ('Is the set version correct for this release?', () => {
+  const releasePackage = {...pkg};
+  delete releasePackage.scripts;
+
+  try {
+    fs.writeFileSync ('dist/package.json', JSON.stringify (releasePackage));
+    fs.copyFileSync ('README.md', 'dist/README.md');
+    fs.copyFileSync ('LICENSE', 'dist/LICENSE');
+
+    console.log ('Packaged');
+  } catch (err) {
+    console.error (err);
+  }
+
+  readline.close ();
 });
